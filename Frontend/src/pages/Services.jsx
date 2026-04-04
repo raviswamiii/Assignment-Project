85697c0677de12aa97530dab1e8e02b8b518e2d2
raviswamiii <raviswamisamsung@gmail.com>
2026-04-04 12:33:00 +0530

    mdLevelRespForServicesPage

--- Frontend/src/pages/Services.jsx
+++ Frontend/src/pages/Services.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import MainText from "../assets/ServicesAssets/heading.svg";
 import text1 from "../assets/ServicesAssets/text1.png";
+import text2 from "../assets/ServicesAssets/text2.png";
+import text3 from "../assets/ServicesAssets/text3.png";
 import arrow from "../assets/ServicesAssets/Arrow 4.svg";
 
 export const Services = () => {
@@ -12,33 +14,33 @@ export const Services = () => {
     },
     {
       paragraph: "The hanger US Air force digital experimental",
-      text: text1,
+      text: text2,
       img: arrow,
     },
     {
       paragraph: "Delta faucet content, social, digital",
-      text: text1,
+      text: text3,
       img: arrow,
     },
   ];
 
   return (
-    <div className="min-h-screen pt-[16vh] flex flex-col items-center gap-[4vh] px-[6vw]">
+    <div className="min-h-screen pt-[16vh] md:pt-[20vh] flex flex-col items-center md:items-start gap-[4vh] px-[6vw]">
       {/* Heading */}
-      <img className="w-[70vw]" src={MainText} alt="Main Text" />
+      <img className="w-[70vw] sm:w-[60vw] md:w-[50vw]" src={MainText} alt="Main Text" />
 
       {/* Services List */}
-      <div className="w-full flex flex-col">
+      <div className="w-full flex flex-col border-t border-gray-400">
         {serviceContainer.map((item, index) => (
-          <div key={index} className="w-full py-[2vh] border-b border-gray-400">
-            <p className="text-xs pb-[1vh]">{item.paragraph}</p>
+          <div key={index} className="md:flex md:items-center md:gap-[4vw] w-full py-[2vh] border-b border-gray-400">
+            <p className="text-xs sm:text-sm pb-[1vh] sm:pb-[2vh] md:w-[22vw]">{item.paragraph}</p>
 
-            <div className="flex items-center justify-between">
+            <div className="flex items-center justify-between md:gap-[4vw]">
               <div className="max-w-[60vw] overflow-hidden">
                 <img className="h-[4vh] w-[80vw]" src={item.text} alt="Text" />
               </div>
 
-              <img className="w-[10vw]" src={item.img} alt="Arrow" />
+              <img className="w-[10vw] md:w-[6vw]" src={item.img} alt="Arrow" />
             </div>
           </div>
         ))}
